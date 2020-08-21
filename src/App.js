a3cfd0d4db82fce822f6f653480548c42f26c481
Daniel Vega <danielalexvega@gmail.com>
2020-08-20 22:18:40 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import Toolbar from './components/Toolbar/Toolbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Toolbar />
+      <main>
+        <p style={{marginTop: '64px'}}>
+          This is the page content!
+        </p>
+      </main>
     </div>
   );
 }
---
+++ src/components/Toolbar/Toolbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './style.css';
+
+const toolbar = props => (
+  <header className="toolbar">
+    <nav className="toolbar_navigation">
+      <div></div>
+      <div className="toolbar_logo"><a href="/">The Logo</a></div>
+      <div className="spacer"></div>
+      <div className="toolbar_navigation-items">
+        <ul>
+          <li><a href="/">Projects</a></li>
+          <li><a href="/">Resume</a></li>
+          <li><a href="/">About Me</a></li>
+          <li><a href="/">Music</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+);
+
+export default toolbar;
